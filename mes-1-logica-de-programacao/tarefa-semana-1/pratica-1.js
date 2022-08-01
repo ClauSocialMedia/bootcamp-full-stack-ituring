7c2524ab5631a740990a7ed1925780503aabfcb7
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-01 20:38:36 -0300

    Meus dados

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -1,5 +1,12 @@
-Nome 
-Email 
-Profissão 
-Idade 
-LugarDosSonhos
+
+let Nome ='Claudia'
+console.log(Nome)
+
+let Email = 'claudiapiurkoski@gmail.com'
+console.log(Email)
+let Profissão = 'Supervisora de Credito'
+console.log(Profissão)
+let Idade = 46
+console.log(Idade)
+let LugarDosSonhos = 'Fernando de Noronha'
+console.log(LugarDosSonhos)
