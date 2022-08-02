c1f53ca2096ebb6389c06236ef7cd45ecce0cf5b
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-01 23:42:39 -0300

    Verdadeiro e falso

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,15 @@
+let Nome 
+let Email 
+let Profissão
+let Idade 
+let LugarSonho
+
+Nome = "Claudia"
+Email = "claudiapiurkoski@gmail.com"
+Idade = 46
+LugarSonho = "Fernando de Noronha"
+
+let IsString = typeof nomedavariavel =="string"
+console.log(true)
+let numero = typeof nomedavariavel =="string"
+console.log(false)
