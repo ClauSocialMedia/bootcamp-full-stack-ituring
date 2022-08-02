0952ae8288bfd4fea7106a7cdab93df7a962914a
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-02 10:02:59 -0300

    saudacao

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -11,6 +11,5 @@ profissao = "Supervisora de vendas"
 idade = 46
 lugarSonho = "Fernando de noronha"
 whats = 99742824
-
-let saudacao = "Olá, ${ nome }! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para $(lugarSonho). Confirma pra gente alguns dados? A sua idade é $(idade), você é $(profissao) e o seu email é $(email)? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
-console. log(saudacao)
+saudacao = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+console.log (saudacao)
