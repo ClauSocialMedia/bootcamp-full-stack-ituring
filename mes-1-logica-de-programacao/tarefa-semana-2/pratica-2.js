667d3c919024ea0c72980a6083e0452593e00d5d
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-07 11:06:26 -0300

    Produto Entregue

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,23 @@
+
+function digaOla(nome) {
+    console.log("Olá " + nome)
+}
+
+digaOla ()
+digaOla ("Joana")
+
+let nomeDoProduto = "Tênis"
+let enderecoDaEntrega = "Rua Curitiba"
+let pessoaQuePegou = "João"
+let nomeDaPessoaQueEnviouEmail = "Nike"
+
+
+
+texto1 = `Fizemos a entrega do produto, ${nomeDoProduto}! no endereço ${enderecoDaEntrega}!, com sucesso!`
+console.log(texto1)
+texto2 = `O seu produto foi recebido por : ${pessoaQuePegou}
+Esperamos que esteja contesnte com seu preoduto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`
+console.log(texto2)
+texto3 = `Atenciosamente
+${nomeDaPessoaQueEnviouEmail}.`
+console.log(texto3)
