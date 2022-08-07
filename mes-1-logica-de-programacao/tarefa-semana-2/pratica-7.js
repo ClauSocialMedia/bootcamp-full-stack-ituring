d9ee3a2d96fb733a5cc68abe0348b74963637c78
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-07 20:08:34 -0300

    Tradução

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,23 @@
+function idiomas(idioma) {
+    if (idioma == "en") {
+        console.log('Hello World!');
+    } else if (idioma == "de") {
+        console.log('Hallo Welt!');
+    }
+    else if (idioma == "fr") {
+        console.log('Bonjour le monde!');
+    } else {
+        console.log('Olá mundo!')
+    }
+
+}
+
+idiomas()
+let idioma = "en"
+
+idiomas()
+idioma = "de"
+
+idiomas()
+idioma = 'fr'
+
---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,9 @@
+let numero1 = 10
+let numero2 = 20
+
+
+if (numero2 === resultadoObtido) {
+    console.log()
+} else {
+    console.log("Tente outra vez!")
+}
