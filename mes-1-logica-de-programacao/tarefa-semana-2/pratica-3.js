015193626d5ac04d35961895000a41cf25dacb7d
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-09 23:54:56 -0300

    Numeros aleatorios

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
@@ -1,8 +1,8 @@
-function numeroAlleatorio(){
-    min = 0
-    max =10000
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function numeroAlleatorio() {
+    const min = 0
+    const max = 10000
+    const numeroEntre0E1000 = Math.floor(Math.random() * (max - min + 1)) + min
+    return numeroEntre0E1000
 }
-
 let numero = numeroAlleatorio()
 console.log(numero)
