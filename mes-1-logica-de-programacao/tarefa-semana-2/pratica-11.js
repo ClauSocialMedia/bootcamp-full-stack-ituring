ece9c8d5fd01d09c5ce3cda3e6549d397615dcfe
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-09 23:44:14 -0300

    Converter cores

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-11.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-11.js
@@ -1,28 +1,35 @@
-const rgb = 'rosa';
-switch (rgb) {
-  case 'rosa': { 
-    const mensagem = 'rgb(255,0,255)'
-        console.log(mensagem);
-    break;
-  } 
-  case 'laranja': { 
-    const laranja = 'laranja';
-    console.log(laranja);
-    break;
-  } 
-  default: { 
-    console.log('rgb(255,255,255)');
-  } 
-}
+function converterCorPraRgb(cor) {
 
+  switch (cor) {
+    case "vermelho":
+      console.log("rgb(255,0,0)");
+      break;
+    case 'laranja':
+      console.log("rgb(255,128,0)");
+      break;
+    case "amarelo":
+      console.log("rgb(255,255,0)");
+      break;
+    case 'verde':
+      console.log("rgb(,255,)");
+      break;
+    case "azukClaro":
+      console.log("rgb(0,255,255)");
+      break;
+    case 'azulMarinho':
+      console.log("rgb(0,0,255)");
+      break;
+    case 'rosa':
+      console.log("rgb(255,0,255)");
+    default: 
+      console.log('rgb(255,255,255)');
+  }
+  }
 
-
-
-let vermelha ="rgb(255,0,0)"
-let laranja = "rgb(255,128,0)"
-let amarela = "rgb(255,255,0)"
-let verde = "rgb(,255,)"
-let azulClaro = "rgb(0,255,255)"
-let azulMarinho = "rgb(0,0,255)"
-let rosa = "rgb(255,0,255)"
-
+  converterCorPraRgb("vermelho")
+  converterCorPraRgb("laranja")
+  converterCorPraRgb("amarelo")
+  converterCorPraRgb("verde")
+  converterCorPraRgb("azulClaro")
+  converterCorPraRgb("azulMarinho")
+  converterCorPraRgb("rosa")
