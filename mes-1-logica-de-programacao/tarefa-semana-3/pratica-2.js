e417ad99f7b41a88ef340bb38e3ed6ec6b874ab9
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-17 18:32:38 -0300

    Primos

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-2.js
@@ -0,0 +1,13 @@
+
+
+function Impar (numero) {
+    if (let i = 0; i <= numero; i++) {
+      for (let i % 2 !== 0) {
+        console.log(i);
+      }
+    }
+  
+  }
+  console. log('Dado de entrada da função: ${"17`"}')
+
+  Impar(17)
