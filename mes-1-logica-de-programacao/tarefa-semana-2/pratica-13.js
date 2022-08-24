9d552a82c6886dbe981abbb486dba9759c283212
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-23 21:14:11 -0300

    numeros

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-13.js
@@ -0,0 +1,16 @@
+function retornaMenorNumero(valor1, valor2) {
+    let menorValor = valor1 < valor2 ? valor1 : valor2
+     
+  /* if (valor1 < valor2) {
+      menorValor = valor1
+  } else {
+        menorValor = valor2
+  }*/
+  
+ return menorValor
+}
+
+console.log(retornaMenorNumero(12, 7))
+
+console.log(0 < 1 ? 'Sim' : 'Não')
+
