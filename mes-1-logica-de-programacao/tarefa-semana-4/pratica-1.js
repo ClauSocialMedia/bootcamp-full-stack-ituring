eb1628c1ff8b19e1d052614142b9ac3f12d481af
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-08-30 16:47:03 -0300

    lista seriados

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-1.js
@@ -0,0 +1,4 @@
+let listaSeriados = ["Breaking Bad", " Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+listaSeriados.forEach(function(nome, índice) {
+    console.log(índice, nome)
+})
